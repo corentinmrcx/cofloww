92f82d2ae64cecccccc0d5e6518eb0bdbe123ad0
Corentin Marcoux <corentinmarcoux51@gmail.com>
2026-04-19 15:06:57 +0200

    feat(deployment): Correction des fichiers pour le déploiement

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+mkdir -p storage/framework/{sessions,views,cache} storage/logs storage/app/public
+
 php artisan package:discover --ansi
 php artisan config:cache
 php artisan route:cache
